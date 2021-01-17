9bd1426d744dd4fb1bc6109656d5a4cf6e3b6f50
Joachim Metz <joachim.metz@gmail.com>
2021-01-17 17:15:58 +0100

    Added end-to-end tests on AppVeyor

--- setup.py
+++ setup.py
@@ -11,6 +11,10 @@ try:
 except ImportError:
   from distutils.core import setup
 
+try:
+  from distutils.command.bdist_msi import bdist_msi
+except ImportError:
+  bdist_msi = None
 
 version_tuple = (sys.version_info[0], sys.version_info[1])
 if version_tuple < (3, 6):
@@ -18,6 +22,22 @@ if version_tuple < (3, 6):
       'Unsupported Python version: {0:s}, version 3.6 or higher '
       'required.').format(sys.version))
   sys.exit(1)
+
+
+if not bdist_msi:
+  BdistMSICommand = None
+else:
+  class BdistMSICommand(bdist_msi):
+    """Custom handler for the bdist_msi command."""
+
+    # pylint: disable=invalid-name
+    def run(self):
+      """Builds an MSI."""
+      # Command bdist_msi does not support the library version, neither a date
+      # as a version but if we suffix it with .1 everything is fine.
+      self.distribution.metadata.version += '.1'
+
+      bdist_msi.run(self)
 
 
 setup(
@@ -29,6 +49,8 @@ setup(
     url='https://github.com/open-source-dfir/dfvfs-snippets',
     maintainer='Open Source DFIR maintainers',
     maintainer_email='open-source-dfir-maintainers@googlegroups.com',
+    cmdclass={
+        'bdist_msi': BdistMSICommand},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
